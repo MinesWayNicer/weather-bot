73fdbeb40b216d0fa89e2332e675c4fbe80d17d7
mineswaynicer <jacksond.davis@gmail.com>
2026-04-19 19:21:50 -0400

    force redeploy fix

--- weather-bot.py
+++ weather-bot.py
@@ -243,4 +243,4 @@ while True:
 
             if location:
                 weather = get_weather(location["lat"], location["lon"])
-                send_message(chat_id, "Hourly update:\n" + format_weather(location, weather))
+                send_message(chat_id, "Hourly update:\n" + format_weather(location, weather))# deploy fix
